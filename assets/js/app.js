a55d0655b0e4fab0dc161913d66f5da38d0d0f34
vanshi2406 <azvanshika27@gmail.com>
2025-09-10 10:57:30 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate-43")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotates-2").classList.toggle("rotate_43")
+    document.body.classList.toggle("overflow-hidden")
+}
